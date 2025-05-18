c20010a226ae3e50892f7d1486c5ee01169856c2
Gilson Just <gilson.just@atos.net>
2025-05-17 23:22:52 -0300

    Added new macros to inform function_name:line_code on log

--- main.cpp
+++ main.cpp
@@ -2,58 +2,13 @@
 
 int main()
 {
-	Logger log("main", logLevel::L_DEBUG);
+	Logger log("Logger", logLevel::L_DEBUG);
 
-	Logger::setTextColor(logColor::C_NORMAL);
-
-	log.debug("debug ", 123);
-	log.error("error");
-	log.info("info");
-
-	Logger::setTextColor(logColor::C_RED);
-	log.warning("warning", " teste2");
-	log.notice("notice");
-
-	log.setLogLevel(logLevel::L_ERROR);
-
-	Logger::setTextColor(logColor::C_GREEN);
-
-	log.debug("debug");
-	log.error("error");
-	log.info("info");
-
-	Logger::setTextColor(logColor::C_BLUE);
-	log.warning("warning");
-	log.notice("notice");
-
-	log.newLine(1);
-	log.newLine(0);
-
-	Logger::setTextColor(logColor::C_RED);
-
-	Logger otherLog("error", logLevel::L_INFO);
-
-	Logger::setTextColor(logColor::C_YELLOW);
-
-	otherLog.error("Error test log");
-	otherLog.debug("Debug test log");
-
-	otherLog.setLogLevel(logLevel::L_ERROR);
-	otherLog.info("Info test log");
-
-	otherLog.newLine(3);
-
-	log.setLogLevel(logLevel::L_INFO);
-
-	Logger::setTextColor(logColor::C_RED);
-
-	log.error("GEJ: ", 0);
-	log.error("GEJ: ", 1);
-	log.error("GEJ: ", 2);
-
-	Logger::setTextColor(logColor::C_NORMAL);
-
-	log.newLine(4);
+	LOG_NOTICE(log, "notice log");
+	LOG_ERROR(log, "logger error", 123);
+	LOG_WARN(log, "logger warning", 123123, " abc ", 321);
+	LOG_INFO(log, "logger info", 321);
+	LOG_DEBUG(log, "Test: ", 123);
 
 	return EXIT_SUCCESS;
 }
